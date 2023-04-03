6d7f8605565b9e20b1e8f009c3139c0b4d275eb3
Andy Dang <andydang@Andys-MacBook-Pro.local>
2023-04-02 20:59:41 -0700

    fix bugs

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ X = np.array(X)
 y = np.array(y)
 
 # Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=14)
 
 # Define the LSTM model
 model = Sequential()
